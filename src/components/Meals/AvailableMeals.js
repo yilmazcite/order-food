9afe257c7e5647702af88ef8cdf9dfe07be5a2d4
Yılmaz <yilmazcite@gmail.com>
2022-08-29 16:20:10 +0300

    retrieve data from Firebase
    Fetch the data from Firebase which was previously hard-coded in this file.

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,39 +1,76 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MealItem from "./MealItem/MealItem";
 import Card from "../UI/Card";
 import "../../styles/Meals.css";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies!",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty.",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty!",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy... and green...",
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
-  //instead of mapping through the dummy data while returning the JSX element,
-  //it is mapped through separately and each item is stored in a variable.
-  const listOfMeals = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://order-food-71bbc-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong. Please try again later.");
+      }
+
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (let key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+    setTimeout(() => {
+      fetchMeals().catch((error) => {
+        setIsLoading(false);
+        setHttpError(error.message);
+      });
+    }, 900);
+  }, []);
+
+  if (isLoading) {
+    const loadingStyle = {
+      textAlign: "center",
+      color: "#ccc",
+      padding: "1rem",
+    };
+
+    return (
+      <section style={loadingStyle}>
+        <h3>Loading... Please wait.</h3>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    const errorStyle = {
+      textAlign: "center",
+      fontSize: "1.5rem",
+      color: "#c23d18",
+    };
+
+    return (
+      <section style={errorStyle}>
+        <h3>{httpError}</h3>
+      </section>
+    );
+  }
+
+  const listOfMeals = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
